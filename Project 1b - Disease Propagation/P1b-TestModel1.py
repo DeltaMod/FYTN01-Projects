301458f86dd44bea3ac3ea33a1491ee16a56bf0f
DeltaMod <33879770+DeltaMod@users.noreply.github.com>
2019-09-06 16:12:04 +0200

    Adding previous changes
    Will be continuing work now

--- Project 1b - Disease Propagation/P1b-TestModel1.py
+++ Project 1b - Disease Propagation/P1b-TestModel1.py
@@ -33,41 +33,80 @@ The differential equation that we will solve per city is:
     dSn/dt = B(\sum^M_m=1{Smn})(\sum^M_m=1{Imn})/(\sum^M_m=1{Nmn}) + g(\sum^M_m=1{Imn}) 
 
 """
+import numpy as np 
+"""
+With numpy, you can use np.array([1,2,3]) and then you can multiply it as you normally couldn't with a list
+numpy also allows you to use np.linspace(i,f,N), which is helpful.
+"""
 import scipy.linalg
-import matplotlib
+import matplotlib.pyplot as plt
+from random import randint
 
-Cities = 3
+cities = 3
 B      = 0.5
+
+
 
 class city(object):
     def __init__(self,N,I,comN):
         self.I   = I
         self.N   = N
         self.S   = N-I
-        self.comN = comN
-        self.comI = []
+        self.comN = comN #Total Number of Commuters FROM the city self.comN[n] is transit from city self to city n 
+        self.comI = []   #      Number of infected FROM the city self.comN[n] is transit from city self to city n
         self.comS = []
-        for var in range(len(comN)):
-            self.comI.append(I/(N)*comN[var])
-            self.comS.append(comN[var]-self.comI[var])
+        if isinstance(comN,int):
+            self.comI = I/(N)*comN
+            self.comS = comN-self.comI
+        if isinstance(comN,list):
+            for var in range(len(comN)):
+                self.comI.append((I/N)*comN[var])
+                self.comS.append(comN[var]-self.comI[var])
+
+# We initialise the cities, and from this can calculate the number of commuters 
+# - We're just going to assume that 10% of each population commutes, and they commute based upon the ratio of the population of each city - so C[0]->C[1] = (C[0].N/10) * C[1].N/(C[0].N+C[1].N+C[2].N) 
+# - When making the real model, we will of course find real values, but for now this will suffice :) 
+Pop = []
+for n in range(cities):
+    Pop.append( randint(int(np.exp(15-1-n)),int(np.exp(15-n))))
+# We then rationalise the number of commuters to each city
+Comm = []
+for n in range(cities):
+    Comm.append([(Pop[n]/10) * Pop[var]/(sum(Pop)) for var in range(cities)])
+    #Then, we find how many people stay in the city - this is just so that our sum later can be sum(all commuters) - (staying) so we only consider travellers
+    Comm[n][n] = Pop[n]
+    for var in range(len(Pop)):
+        if var !=n:
+            Comm[n][n] = Comm[n][n] - Comm[n][var]
+
+#Now we generalise all cities into a single dictionary command, callable by C[index].classobjects 
+C = {n:city(Pop[n],10,Comm[n]) for n in range(cities)}
+
+
+Temp1 = []
+dI    = []
+#ICS is "infected city state", and will store [t, I]
+ICS   = []
+#We do not need to make SCS, since C[index] = N - ICS, but we will store it later 
+
+for t in range(100):
+    dI.append([B*C[n].I*C[n].S/C[n].N + sum([C[var].comN[n] for var in range(cities)]) - C[n].comN[n] for n in range(cities)])
+     for var in range(cities):
+        ICS
+        City[var] = city(C[var].N,  C[var].I, [C[var].comN[n] for n in range(cities)])
         
-N1 = 10000; N2  = 10000; N3 = 100000; 
-C11 = 9980; C12 =    10; C13 = 10;
-C21 =   10; C22 =  9980; C23 = 10;    
-C31 =   10; C32 =    10; C33 = 99980;       
-C1 = city(N1,100,[C11,C12,C13])
-C2 = city(N2,  0,[C21,C22,C23])
-C3 = city(N3,  0,[C31,C32,C33])
-
-
-#for var in range(Cities):#
-for var in range(100):
-    dI1 = B*(C1.I-C1.comI[1]-C1.comI[2]+C2.comI[0]+C3.comI[0])*(C1.S-C1.comS[1]-C1.comS[2]+C2.comS[0]+C3.comS[0])/(C1.comN[0]+C2.comN[0]+C3.comN[0]) 
-    rat = dI1/(C1.comN[0]+C2.comN[0]+C3.comN[0])
-    C1.I = C1.I+C1.N*rat; C2.I = C2.I+C2.comN[0]*rat; C3.I = C3.I+C3.comN[0]*rat;
-
-    C1 = city(N1,  C1.I,[C11,C12,C13])
-    C2 = city(N2,  C2.I   ,[C21,C22,C23])
-    C3 = city(N3,  C3.I   ,[C31,C32,C33])
+print(str(t))
+        
+"""  
+    dI1 = B*(C[0].I-C[0].comI[1]-C[0].comI[2]+C[1].comI[0]+C[2].comI[0])*(C[0].S-C[0].comS[1]-C[0].comS[2]+C[1].comS[0]+C[2].comS[0])/(C[0].comN[0]+C[1].comN[0]+C[2].comN[0]) 
+    rat = dI1/(C[0].comN[0]+C[1].comN[0]+C[2].comN[0])
+    C[0].I = C[0].I+C[0].N*rat; C[1].I = C[1].I+C[1].comN[0]*rat; C[2].I = C[2].I+C[2].comN[0]*rat;
+    for var in range(cities):
+        C[var] = city(C[var].N,  C[var].I, [C[var].comN[n] for n in range(cities)])
     
-    print('C1.I = ', str(C1.I))
+    print('C[0].I = ', str(C[0].I))
+    Temp1.append(C[0].I) 
+    
+plt.plot(Temp1)
+plt.show()
+"""
