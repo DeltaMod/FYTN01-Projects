fcd6989202fefbe9013b41ddfabe4bab42f7f7f2
Stéphan <stephanpissot@gmail.com>
2019-09-06 15:41:18 +0200

    Improved graphing, added total-flow check loop

--- Project 1b - Disease Propagation/P1b-ModelSIR_withVaccine.py
+++ Project 1b - Disease Propagation/P1b-ModelSIR_withVaccine.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import math
 import numpy as np
 import scipy.integrate
 
@@ -11,8 +12,10 @@ gamma = 0.1
 
 #population parameters
 #TODO : find some real cities to work from
-susceptible_0 = [1000, 20] #initial susceptible ppl (population)
-infected_0 = [100, 0] #initial infected ppl
+# Note : the following three lists MUST have the same lenght (the number of cities)
+cities_names = ['Onetown', 'Twoburg', 'Threeville', 'Fourstad']
+susceptible_0 = [1000, 20, 300, 700] #initial susceptible ppl (population)
+infected_0 = [100, 0, 0, 0] #initial infected ppl
 
 #integration parameter
 t= np.linspace(0,50,300) #integration range : start, end, number of points
@@ -24,7 +27,17 @@ recovered_0 = [0]*nb_cities #at first, no recovered ppl
 state_0 = susceptible_0 + infected_0 + recovered_0 #pack the lists in one long list
 
 w = [[0.01]*nb_cities]*nb_cities #probality for someone to go from one city to another.
-# TODO : make it dependent on the size of the origin and destination cities
+# TODO : make it dependent on the size of the origin and destination cities, also, make sure total flow is null for each city
+#so we don't get massive migrations like it seems to be the case in this model
+
+#Quick run through the list to check that the cities are not leaking to many people :
+for n in range(nb_cities):
+    tot_flux = 0
+    for m in range(nb_cities):
+        if m!=n :
+            tot_flux += w[m][n]*(susceptible_0[n]+infected_0[n]) - w[n][m]*(susceptible_0[m]+infected_0[m])
+    if tot_flux != 0:
+        print('Warning. With the current setting of w, the flow of people out of '+cities_names[n]+' is of '+str(tot_flux)+' every day.')
 
 
 def SIR(state, t):
@@ -62,14 +75,35 @@ sol_infect = sol[:,nb_cities:2*nb_cities]#unpacking solution list
 sol_recov = sol[:,2*nb_cities:]#unpacking solution list
 
 # plot the solution :
-for n in range(nb_cities):
-    plt.plot(t, sol_infect[:,n], label='infected in city n'+str(n))
-    plt.plot(t, sol_suscept[:,n], label='suscept in city n'+str(n))
-    plt.plot(t, sol_recov[:,n], label='recov in city n'+str(n))
-plt.legend()
-plt.show()
-# TODO : change that and make subplots for each city or each variable(S,I,R)
+#for n in range(nb_cities):
+#    plt.plot(t, sol_infect[:,n], label='infected in city n'+str(n))
+#    plt.plot(t, sol_suscept[:,n], label='suscept in city n'+str(n))
+#    plt.plot(t, sol_recov[:,n], label='recov in city n'+str(n))
+#plt.legend()
+#plt.show()
 
+#for n in range(nb_cities):
+#    plt.plot(t, sol_infect[:,n], label='infected in city n'+str(n))
+#    plt.plot(t, sol_suscept[:,n], label='susceptible in city n'+str(n))
+#    plt.plot(t, sol_recov[:,n], label='recovered in city n'+str(n))
+#    plt.title('Disease-spreading in city n'+str(n))
+#    plt.legend()
+#    plt.show()
 
 #print(sol[0]) #DEBUG
 #print(sol[-1])#DEBUG
+
+figsize = (10, 8)
+cols = math.ceil(math.sqrt(nb_cities))
+rows = math.ceil(math.sqrt(nb_cities))
+#cols = 1
+#rows = nb_cities 
+fig1, axs = plt.subplots(rows, cols, figsize=figsize, constrained_layout=True)
+axs = axs.flatten()
+for n in range(nb_cities):
+    axs[n].plot(t, sol_infect[:,n], label='Infected in '+cities_names[n], color='red')
+    axs[n].plot(t, sol_suscept[:,n], label='Susceptible in '+cities_names[n], color='blue')
+    axs[n].plot(t, sol_recov[:,n], label='Recovered in '+cities_names[n], color='green')
+    axs[n].set_title('Disease-spreading in '+cities_names[n])
+    axs[n].legend()
+plt.show()
