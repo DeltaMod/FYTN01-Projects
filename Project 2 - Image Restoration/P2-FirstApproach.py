0e8185f889b701666f7c9c82ed961f1b876bd8d2
Stéphan <stephanpissot@gmail.com>
2019-09-24 15:50:50 +0200

    Update P2-FirstApproach.py

--- Project 2 - Image Restoration/P2-FirstApproach.py
+++ Project 2 - Image Restoration/P2-FirstApproach.py
@@ -9,13 +9,14 @@ plt.imshow(original, cmap='gray')
 plt.title('Original Image')
 plt.show()
 
+##
 plt.figure()
 plt.imshow(mask, cmap='gray')
 plt.title('Mask')
 plt.show()
 
 ##
-damaged_img = original*(1-mask)
+damaged_img = original*(1-(mask/255))
 plt.figure()
 plt.imshow(damaged_img, cmap='gray')
 plt.title('Damaged image')
