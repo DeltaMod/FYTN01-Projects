c61de7ab5eb2736eaa8e5feeedad999601563559
Stéphan <stephanpissot@gmail.com>
2019-09-24 15:37:24 +0200

    update

--- Project 2 - Image Restoration/P2-FirstApproach.py
+++ Project 2 - Image Restoration/P2-FirstApproach.py
@@ -1,3 +1,15 @@
-from scipy.misc import imread
+from matplotlib.pyplot import imread
+import matplotlib.pyplot as plt
 
+original = imread('EM_ScreamGS.bmp')[:,:,0]
+mask = imread('mask.bmp')[:,:,0]
 
+plt.figure()
+plt.imshow(original, cmap='gray')
+plt.title('Original Image')
+plt.show()
+
+plt.figure()
+plt.imshow(mask, cmap='gray')
+plt.title('Mask')
+plt.show()
