df19d5d6fc0eaf24ef99babcdc42602fdd47bc32
DeltaMod <33879770+DeltaMod@users.noreply.github.com>
2019-10-19 20:56:43 +0200

    Many changes were made - Progress bar has been added, to make sure everything works as intended
    Plotting modes, alternate "starvation" calculation, as well as population regulation to prevent the simulation from blowing up have been added

--- Project 3 - Stochastic Processes/P3-Main.py
+++ Project 3 - Stochastic Processes/P3-Main.py
@@ -8,27 +8,56 @@ import cv2 #run pip install opencv-python
 import time 
 from random import randint
 from mpl_toolkits.mplot3d import Axes3D
-
-
-NWALK  = 50   #Number of Walkers
-DIMX = 100; DIMY = 100; DIMZ = 100#Dimension of Area Considered 
-NSTEPS      = 500
-PGDIM       = [DIMX,DIMY,DIMZ] #Plagground Dimensions [x,y,z]
-WalkerType  = 'Aggro' #Aggro|Exploding
-BIRTHS      = 'True'  #Births or no Births
-AGGRNG        = 6       #Aggro Gen RNG - 1:AGGRNG+1 chance to make hunter 
+import tqdm
 plt.rcParams['figure.dpi']   = 150
 
+#Good parameters
+#NWALK = 500   #Number of Walkers
+#DIMX  = 100; DIMY = 100; DIMZ = 100#Dimension of Area Considered 
+#NSTEPS     = 3000
+#PGDIM      = [DIMX,DIMY,DIMZ] #Plagground Dimensions [x,y,z]
+#WalkerType = 'Aggro' #Aggro|Exploding
+#BIRTHS     = 'True'  #Births or no Births
+#AGGRNG     = 6       #Aggro Gen RNG - 1:AGGRNG+1 chance to make hunter 
+#HR         = 100     #Hunting Radius
+#PR         = 50      #Passive Radius
+#BR         = 200     #Passive Birth Rate (equiv to 1:BR)
+#ABR        = 10      #Aggro Birth chance (equiv to 1:ABR) - This can only happen within 2 days of eating
+#ASTRV      = 50      #Rate of aggressive starvation - If aggro does not eat in  ASTRV days, it dies
+#PSTRV      = BR*1.5  #Rate of passive "starvation" - On average, each passive cell should reproduce twice in its lifetime
+#ANIMATE    = False   #Animates results - set to false to simply get the population plot
+#AXLIM      = True    #If true - sets axis constraints for entire lattice, if false - sets constraints only to the action
+#MAXPAS     = NWALK*5 #Maximum sustainiable population - This is just to make sure the simulation does not get out of hand
 
+
+NWALK = 500   #Number of Walkers
+DIMX  = 50; DIMY = 50; DIMZ = 50#Dimension of Area Considered 
+NSTEPS     = 5000
+PGDIM      = [DIMX,DIMY,DIMZ] #Plagground Dimensions [x,y,z]
+WalkerType = 'Exploding' #Aggro|Exploding
+BIRTHS     = False  #Births or no Births
+AGGRNG     = 99999999       #Aggro Gen RNG - 1:AGGRNG+1 chance to make hunter 
+HR         = 5       #Hunting Radius
+PR         = 2       #Passive Radius
+BR         = 50      #Passive Birth Rate (equiv to 1:BR)
+ABR        = 50     #Aggro Birth chance (equiv to 1:ABR) - This can only happen within 2 days of eating
+ASTRV      = 16      #Rate of aggressive starvation - If aggro does not eat in  ASTRV days, it dies
+PSTRV      = BR*3  #Rate of passive "starvation" - On average, each passive cell should reproduce twice in its lifetime
+ANIMATE    = False    #Animates results - set to false to simply get the population plot
+AXLIM      = True    #If true - sets axis constraints for entire lattice, if false - sets constraints only to the action
+MAXPAS     = NWALK*5 #Maximum sustainiable population - This is just to make sure the simulation does not get out of hand
+DIMPLOT    = False #Plots only distribution
 #%% Testing out using lists of lists instead for this, such that W[n,0]
 
 def walkergen(N,DIM,TYPE,HRNG):
     WGen = [None]*N
-    LBM = 4/10; UBM = 6/10; 
+    LBM = 0/10; UBM = 10/10; 
     LBX = int(LBM*DIM[0]);LBY = int(LBM*DIM[1]);LBZ = int(LBM*DIM[2])
     UBX = int(UBM*DIM[0]);UBY = int(UBM*DIM[1]);UBZ = int(UBM*DIM[2])
     for m in range(N):
         HNTR = bool(randint(0,HRNG))
+        if TYPE=='Exploding':
+            HNTR = True
         if HNTR ==False:
             X    = randint(1,DIM[0]-1) 
             Y    = randint(1,DIM[1]-1) 
@@ -44,13 +73,14 @@ def walkergen(N,DIM,TYPE,HRNG):
     WStep.append([WGen[n] for n in range(len(WGen))])
     return(WStep)
     
-def walkeradd(self,BIRA):
-    for n in range(len(self[-1])):
-        if self[-1][n][4] == 'passive' and self[-1][n][3] == 'alive':
-            BRNG = bool(int(randint(0,BIRA)/BIRA))
-            if BRNG == True:
-                coordmod = [self[-1][n][1][0]+randint(-1,1),self[-1][n][1][1]+randint(-1,1),self[-1][n][1][2]+randint(-1,1)]
-                self[-1].append([len(self[-1])+1,coordmod,self[-1][n][2],'alive','passive',[],0])
+def walkeradd(self,BIRA,LPAS,MAXPOP):
+    if len(LPAS[-1])<MAXPOP:
+        for n in range(len(self[-1])):
+            if self[-1][n][4] == 'passive' and self[-1][n][3] == 'alive':
+                BRNG = bool(int(randint(0,BIRA)/BIRA))
+                if BRNG == True:
+                    coordmod = [self[-1][n][1][0]+randint(-1,1),self[-1][n][1][1]+randint(-1,1),self[-1][n][1][2]+randint(-1,1)]
+                    self[-1].append([len(self[-1])+1,coordmod,self[-1][n][2],'alive','passive',[],0])
 
 def walkermove(self):
     WMov = []
@@ -158,15 +188,26 @@ def walkerkill(self,PIND,PLOC,AIND,ALOC):
             W[-1][PIND[-1][IDLOC[n][0]]][3] = 'dead'
             W[-1][AIND[-1][IDLOC[n][1]]][6] = 0   #set days since last eaten
 
-def aggrostarve(self,AIND):
+def aggrostarve(self,AIND,STARVD):
     for n in range(len(AIND[-1])):
-        if self[-1][AIND[n]][6] > 100:
-            self[-1][AIND[n]][3][3] = 'dead'
-            
-def aggrobirth(self,AIND):
+        if self[-1][AIND[-1][n]][6] > STARVD:
+            self[-1][AIND[-1][n]][3] = 'dead'
+
+def passivestarve(self,PIND,STARVD,PASMAX):
+    CROWDING = bool(randint(0,int((PASMAX/(len(PIND[-1])+1))) ))
+    for n in range(len(PIND[-1])):
+        if self[-1][PIND[-1][n]][6] > STARVD:
+            self[-1][PIND[-1][n]][3] = 'dead'
+        elif CROWDING == False and self[-1][PIND[-1][n]][6]<2:
+            self[-1][PIND[-1][n]][3] = 'dead'
+          
+def aggrobirth(self,AIND,BIRA):
     for n in range(len(AIND[-1])):
-        if self[-1][AIND[n]][6] > 5:
-            self[-1][AIND[n]][3][3] = 'dead'
+        if self[-1][AIND[-1][n]][6] < 2:
+            BRNG = bool(int(randint(0,BIRA)/BIRA))
+            if BRNG == True:
+                coordmod = [self[-1][AIND[-1][n]][1][0]+randint(-1,1),self[-1][AIND[-1][n]][1][1]+randint(-1,1),self[-1][AIND[-1][n]][1][2]+randint(-1,1)]
+                self[-1].append([len(self[-1])+1,coordmod,self[-1][AIND[-1][n]][2],'alive','aggro',[],0])
 #%%
     
                 
@@ -193,28 +234,38 @@ def NearestPassive(LAGG,LPASS,nselec):
     delt  = []
     NLOC  = []
     APVec = []
-    for n in range(len(LAGG[nselec])):
-        delt.append(abs(LAGG[nselec][n]-LPASS[nselec]))
-    for n in range(len(delt)):
-        NSUM.append([sum(delt[n][m]) for m in  range(len(LPASS[nselec]))])
-        NLOC.append(np.array(LPASS[nselec][np.argmin(NSUM[n])]))
-        APVec.append([LAGG[nselec][n],NLOC[n]])
+    if len(LPASS[nselec])!= 0:
+        for n in range(len(LAGG[nselec])):
+            delt.append(abs(LAGG[nselec][n]-LPASS[nselec]))
+        for n in range(len(delt)):
+            NSUM.append([sum(delt[n][m]) for m in  range(len(LPASS[nselec]))])
+            NLOC.append(np.array(LPASS[nselec][np.argmin(NSUM[n])]))
+            APVec.append([LAGG[nselec][n],NLOC[n]])
+    else:
+        for n in range(len(LAGG[nselec])):
+            APVec.append([np.array((LAGG[nselec][n])),np.array((-1000, -1000, -1000))])      
     return APVec
+
     #return delt
 def NearestAggro(LAGG,LPASS,nselec):
     NSUM  = []
     delt  = []
     NLOC  = []
     PAVec = []
-    for n in range(len(LPASS[nselec])):
-        delt.append(abs(LPASS[nselec][n]-LAGG[nselec]))
-    for n in range(len(delt)):
-        NSUM.append([sum(delt[n][m]) for m in  range(len(LAGG[nselec]))])
-        NLOC.append(np.array(LAGG[nselec][np.argmin(NSUM[n])]))
-        PAVec.append([LPASS[nselec][n],NLOC[n]])
+    if len(LAGG[nselec])!=0:
+        for n in range(len(LPASS[nselec])):
+            delt.append(abs(LPASS[nselec][n]-LAGG[nselec]))
+        for n in range(len(delt)):
+            NSUM.append([sum(delt[n][m]) for m in  range(len(LAGG[nselec]))])
+            NLOC.append(np.array(LAGG[nselec][np.argmin(NSUM[n])]))
+            PAVec.append([LPASS[nselec][n],NLOC[n]])
+    else:
+        for n in range(len(LPASS[nselec])):
+            PAVec.append([np.array((LPASS[nselec][n])),np.array((-1000, -1000, -1000))])
+    
     return PAVec
 
-W = walkergen(NWALK,PGDIM,'basic',AGGRNG)
+W = walkergen(NWALK,PGDIM,WalkerType,AGGRNG)
 AlvLoc = [] #Locations of Alive Walkers
 AlvIND = [] #Location Index of Alive Walkers
 DthLoc = [] #Locations of Dead Walkers
@@ -224,83 +275,149 @@ PasIND = [] #Location Index of passive walkers
 PasLoc = [] #Location of PassiveWalkers
 ATPVec = [] #Vector from Agg->Pass
 PTAVec = [] #Vector from Pass->Agg
-HR     = 50 #Hunting Radius
-PR     = 20 #Passive Radius
-BR     = 250 #Birth Rate (equiv to 1:BR)
-for m in range(NSTEPS):
-    if BIRTHS == 'True':      
-        walkeradd(W,BR)
-    LocLivAll = localive(W)
-    AlvLoc.append(np.array(LocLivAll[1]))
-    AlvIND.append(np.array(LocLivAll[0]))
-    DthLoc.append(np.array(LocLivAll[2]))
-    AggIND.append(np.array(LocLivAll[3]))
-    AggLoc.append(np.array(LocLivAll[4]))
-    PasIND.append(np.array(LocLivAll[5]))
-    PasLoc.append(np.array(LocLivAll[6]))
-    
-    if WalkerType == 'Aggro':
-        if len(PasLoc[-1])!=0:
-            ATPVec.append(NearestPassive(AggLoc,PasLoc,-1))
-            PTAVec.append(NearestAggro(AggLoc,PasLoc,-1))
-            for n in range(len(AggIND[-1])):
-                W[-1][AggIND[-1][n]][5] = ATPVec[-1][n]
-            for n in range(len(PasIND[-1])):
-                W[-1][PasIND[-1][n]][5] = PTAVec[-1][n]
-            
-            walkerhunt(W,HR,PR)
-            walkerkill(W,PasIND,PasLoc,AggIND,AggLoc)
-        else: 
+pbstep = 100 #ProgressBarSteps
+with tqdm.tqdm(total=int(pbstep)) as pbar:
+    for m in range(NSTEPS):
+        if BIRTHS == True and m!=0:      
+            walkeradd(W,BR,PasIND,MAXPAS)
+        LocLivAll = localive(W)
+        AlvLoc.append(np.array(LocLivAll[1]))
+        AlvIND.append(np.array(LocLivAll[0]))
+        DthLoc.append(np.array(LocLivAll[2]))
+        AggIND.append(np.array(LocLivAll[3]))
+        AggLoc.append(np.array(LocLivAll[4]))
+        PasIND.append(np.array(LocLivAll[5]))
+        PasLoc.append(np.array(LocLivAll[6]))
+        
+        if WalkerType == 'Aggro':
+            if len(PasLoc[-1])!=0:
+                ATPVec.append(NearestPassive(AggLoc,PasLoc,-1))
+                PTAVec.append(NearestAggro(AggLoc,PasLoc,-1))
+                for n in range(len(AggIND[-1])):
+                    W[-1][AggIND[-1][n]][5] = ATPVec[-1][n]
+                for n in range(len(PasIND[-1])):
+                    W[-1][PasIND[-1][n]][5] = PTAVec[-1][n]
+                
+                walkerhunt(W,HR,PR)
+                walkerkill(W,PasIND,PasLoc,AggIND,AggLoc)
+                aggrostarve(W,AggIND,ASTRV)
+                aggrobirth(W,AggIND,ABR)
+                passivestarve(W,PasIND,PSTRV,MAXPAS)
+                
+            else: 
+                walkermove(W)
+                aggrostarve(W,AggIND,50)
+                aggrobirth(W,AggIND,ABR)
+                passivestarve(W,PasIND,PSTRV,MAXPAS)
+        elif WalkerType == 'Exploding':
             walkermove(W)
-    elif WalkerType == 'Exploding':
-        walkermove(W)
-    
-    #walkersplode(W,AlvLoc,AlvIND)
-
+            walkersplode(W,AlvLoc,AlvIND)
+        if m%int(NSTEPS/pbstep)==0:
+            pbar.update(1)
+            pbar.set_description('Hunters: '+str(len(AggIND[-1]))+', Runners: '+str(len(PasIND[-1])))
+        
+pbar.close()
 #%% Colourfader code
 def colorFader(c1,c2,mix=0): #fade (linear interpolate) from color c1 (at mix=0) to c2 (mix=1)
     c1=np.array(mpl.colors.to_rgb(c1))
     c2=np.array(mpl.colors.to_rgb(c2))
     return mpl.colors.to_hex((1-mix)*c1 + mix*c2)
 #%%
-fig = plt.figure(1)
-plt.ion()
-DW = [NWALK-(NWALK-len(AlvLoc[n])) for n in range(len(AlvLoc))]
-axis = 'lim'
-for n in range(NSTEPS):
+DW  =  [NWALK-(NWALK-len(AlvLoc[n])) for n in range(len(AlvLoc))]
+DAgg = [len(AggIND[n]) for n in range(len(AggIND))]
+DPas = [len(PasIND[n]) for n in range(len(PasIND))]
+   
+if ANIMATE == True:
+    fig = plt.figure(1)
+    plt.ion()
+    for n in range(NSTEPS):
+        fig.clf()
+        fig.suptitle('Evolution of random walkers')
+        if DIMPLOT == True:
+            ax = fig.add_subplot(1,2,1,projection='3d')
+            ax = fig.gca(projection='3d')
+            if AXLIM == True:
+                ax.set_xlim3d(0, PGDIM[0])
+                ax.set_ylim3d(0,PGDIM[1])
+                ax.set_zlim3d(0,PGDIM[2])
+            if len(PasLoc[n])!=0:
+                ax.scatter3D(PasLoc[n][:,0],PasLoc[n][:,1],PasLoc[n][:,2],color='b',alpha=0.8)
+            if len(AggLoc[n])!=0:
+                ax.scatter3D(AggLoc[n][:,0],AggLoc[n][:,1],AggLoc[n][:,2],color='r',alpha=0.8)
+            if len(DthLoc[n])!=0:
+                 ax.scatter3D(DthLoc[n][:,0],DthLoc[n][:,1],DthLoc[n][:,2],color='k',alpha=0.2)
+            if len(ATPVec) > n:
+                if len(ATPVec[n])!=0:
+                    
+                    for m in range(len(ATPVec[n])):
+                        if sum(ATPVec[n][m][1])!=-3000:
+                            if sum(abs(ATPVec[n][m][1]-ATPVec[n][m][0]))<HR:
+                                linecolour = 'orange'
+                            elif sum(abs(ATPVec[n][m][1]-ATPVec[n][m][0]))>HR:
+                                linecolour = 'green'
+                            ax.plot3D([ATPVec[n][m][0][0],ATPVec[n][m][1][0]],[ATPVec[n][m][0][1],ATPVec[n][m][1][1]],[ATPVec[n][m][0][2],ATPVec[n][m][1][2]],color = linecolour )
+            ax.set_xlabel('X')
+            ax.set_ylabel('Y')
+            ax.set_zlabel('Z')
+            ax = fig.add_subplot(1,2,2)
+        else:
+            ax = fig.gca()
+        if AXLIM == True:
+            ax.axis((0,NSTEPS,0,max(DW)))
+        ax.plot(DW[1:n],color='green')
+        ax.plot(DAgg[1:n],color='red')
+        ax.plot(DPas[1:n],color='blue')
+        ax.yaxis.tick_right()
+        ax.yaxis.set_label_position("right")
+        ax.set_ylabel('Number of walkers')
+        ax.set_xlabel('Number of iterations')
+        plt.pause(0.01)
+else:
+    fig = plt.figure(1)
+    plt.ion()
     fig.clf()
     fig.suptitle('Evolution of random walkers')
-    ax = fig.add_subplot(1,2,1,projection='3d')
-    ax = fig.gca(projection='3d')
-    if axis == 'lim':
-        ax.set_xlim3d(0, PGDIM[0])
-        ax.set_ylim3d(0,PGDIM[1])
-        ax.set_zlim3d(0,PGDIM[2])
-    if len(PasLoc[n])!=0:
-        ax.scatter3D(PasLoc[n][:,0],PasLoc[n][:,1],PasLoc[n][:,2],color='b',alpha=0.8)
-    if len(AggLoc[n])!=0:
-        ax.scatter3D(AggLoc[n][:,0],AggLoc[n][:,1],AggLoc[n][:,2],color='r',alpha=0.8)
-    if len(DthLoc[n])!=0:
-         ax.scatter3D(DthLoc[n][:,0],DthLoc[n][:,1],DthLoc[n][:,2],color='k',alpha=0.2)
-    if len(ATPVec) > n:
-        if len(ATPVec[n])!=0:
-            
-            for m in range(len(ATPVec[n])):
-                if sum(abs(ATPVec[n][m][1]-ATPVec[n][m][0]))<HR:
-                    linecolour = 'orange'
-                else:
-                    linecolour = 'green'
-                ax.plot3D([ATPVec[n][m][0][0],ATPVec[n][m][1][0]],[ATPVec[n][m][0][1],ATPVec[n][m][1][1]],[ATPVec[n][m][0][2],ATPVec[n][m][1][2]],color = linecolour )
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    ax.set_zlabel('Z')
-    ax = fig.add_subplot(1,2,2)
-    ax.plot(DW[1:n])  
+    if DIMPLOT == True:
+        ax = fig.add_subplot(1,2,1,projection='3d')
+        ax = fig.gca(projection='3d')
+        if AXLIM == True:
+            ax.set_xlim3d(0, PGDIM[0])
+            ax.set_ylim3d(0,PGDIM[1])
+            ax.set_zlim3d(0,PGDIM[2])
+        if len(PasLoc[-1])!=0:
+            ax.scatter3D(PasLoc[-1][:,0],PasLoc[-1][:,1],PasLoc[-1][:,2],color='b',alpha=0.8)
+        if len(AggLoc[-1])!=0:
+            ax.scatter3D(AggLoc[-1][:,0],AggLoc[-1][:,1],AggLoc[-1][:,2],color='r',alpha=0.8)
+        if len(DthLoc[-1])!=0:
+             ax.scatter3D(DthLoc[-1][:,0],DthLoc[-1][:,1],DthLoc[-1][:,2],color='k',alpha=0.2)
+        if len(ATPVec) > n:
+            if len(ATPVec[-1])!=0:
+                
+                for m in range(len(ATPVec[-1])):
+                    if sum(ATPVec[-1][m][1])!=-3000:
+                        if sum(abs(ATPVec[-1][m][1]-ATPVec[-1][m][0]))<HR:
+                            linecolour = 'orange'
+                        elif sum(abs(ATPVec[-1][m][1]-ATPVec[-1][m][0]))>HR:
+                            linecolour = 'green'
+                        ax.plot3D([ATPVec[-1][m][0][0],ATPVec[-1][m][1][0]],[ATPVec[-1][m][0][1],ATPVec[-1][m][1][1]],[ATPVec[-1][m][0][2],ATPVec[-1][m][1][2]],color = linecolour )
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+        ax = fig.add_subplot(1,2,2)
+    else:
+            ax = fig.gca()
+    if AXLIM == True:
+        ax.axis((0,NSTEPS,0,max(DW)))
+    ax.plot(DW,color='green',label='Total')
+    ax.plot(DAgg,color='red',label='Hunters')
+    ax.plot(DPas,color='blue',label='Runners')
     ax.yaxis.tick_right()
     ax.yaxis.set_label_position("right")
     ax.set_ylabel('Number of walkers')
     ax.set_xlabel('Number of iterations')
     plt.pause(0.01)
+    ax.legend()
+    ax.grid()
 #%% 
 
    
